fbc1c704a0d89591baed4fefec389307772f253f
wherein-io <64546106+wherein-io@users.noreply.github.com>
2020-11-02 17:31:11 +0800

    Update config.js

--- book/.vuepress/config.js
+++ book/.vuepress/config.js
@@ -5,8 +5,8 @@ module.exports = {
     sidebar: {
       '/': [
         'dapp_incubator',
-        'features',
         'nutbox',
+        'features',
         'ecosystem',
         'roadmap',
         'contract_account',
