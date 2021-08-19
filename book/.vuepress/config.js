48e3b42f435f9f592db3872fc15de97157607871
wangxi <wangxi8206@sina.com>
2021-08-19 14:45:33 +0800

    Update config.js

--- book/.vuepress/config.js
+++ book/.vuepress/config.js
@@ -30,7 +30,6 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'White Paper', link: '/white_paper/' },
-      { text: 'Technical Paper', link: '/technical_paper/' },
       { text: 'Peanut Lite Paper', link: '/lite_paper_v1/' }
     ],
     displayAllHeaders: true,
